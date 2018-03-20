791020312b36b5b5964567a5383fe7e3329d20d3
toxin <yuri.belousov.1998@gmail.com>
2018-03-20 20:27:18 +0300

    add MEMATOR

---
+++ src/main/java/com.toxin.jbot/Memator.java
@@ -0,0 +1,52 @@
+package com.toxin.jbot;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Random;
+
+public class Memator {
+
+    private static final String SOURCE = "http://pikchi.net";
+    private static final String PATH = "/random.html";
+    private static final String KEY_WORDS = "/uploads";
+    private static final String RES = "src/main/resources/img/";
+
+    private static final Random RANDOM = new Random();
+
+    public static File getMem() {
+        try {
+
+            URL url = new URL(SOURCE + PATH);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+            String parse, link = "";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            while (reader.ready()) {
+                parse = reader.readLine();
+                if (parse.contains(KEY_WORDS)) {
+                    int from = parse.indexOf(KEY_WORDS);
+                    int to = parse.indexOf("\"", from);
+                    link = SOURCE + parse.substring(from, to);
+                    break;
+                }
+            }
+
+            try(InputStream in = new URL(link).openStream()) {
+                Files.copy(in, Paths.get(RES + "mem_" + RANDOM.nextInt(1000) + ".jpg"));
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new File(RES);
+    }
+
+}
